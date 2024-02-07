0563fca62b013b78a14ed8af9899baeccd9c1f30
NoelHa28 <n.d.r.haarman@student.rug.nl>
2024-02-07 16:58:57 +0000

    New structure

--- compile.cmd
+++ compile.cmd
@@ -1,5 +1,9 @@
+@echo off
+REM Create the bin directory if it does not exist
+if not exist ".\bin" mkdir .\bin
+
 REM Compile the java source files from the src directory and place the output in the bin directory
-javac .\*.java
+javac -d .\bin -sourcepath .\src .\src\*.java
 
 REM Echo a success message
 echo Compilation successful.
--- run.cmd
+++ run.cmd
@@ -1,6 +1,6 @@
 @echo off
 REM Run the main class from the bin directory
-java -cp ./ Main
+java -cp .\bin Main
 
 REM Echo a success message
 echo Program executed.
